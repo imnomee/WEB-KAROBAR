660f8e46a5ee5df88062978073fad1a9e9b5fc94
Noman Rafiq <heyimnomee@gmail.com>
2025-02-10 20:08:32 +0500

    test

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <div className="container p-8">
+            <button className="bg-blue-400 px-4 py-8 rounded-full text-white hover:text-gray-600">
+                Hover Me to check the outcome
+            </button>
+        </div>
+    );
+}
+
+export default App;
